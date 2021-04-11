a325eb61c8e7ea5cc9d140f8358821fd260a993a
Ariel2294 <arielopez229422@gmail.com>
2021-04-11 10:29:13 -0600

    Agregando Gestiones activas

--- src/pages/Informes/GestionesInformes/GestionesInformes.js
+++ src/pages/Informes/GestionesInformes/GestionesInformes.js
@@ -4,6 +4,7 @@ import { Tabs, Tab } from "react-bootstrap";
 import LayoutPanelEmpty from "../../../components/layouts/panels/LayoutPanelEmpty";
 import Request from "../../../services/Request";
 import HTTP from "../../../helpers/HTTP";
+import Alerts from "../../../services/Alerts";
 export default class GestionesInformes extends Component {
   constructor(props) {
     super(props);
@@ -13,6 +14,7 @@ export default class GestionesInformes extends Component {
   inicial_state = {
     tab_active: "gestion_activa",
     gestiones_nopublicadas: [],
+    gestiones_publicadas: [],
   };
   async handleChangeTab(key) {
     await this.setState({ tab_active: key });
@@ -24,6 +26,7 @@ export default class GestionesInformes extends Component {
   }
   componentDidMount() {
     this.getGestionesNoPublicadas();
+    this.getGestionesPublicadas();
   }
 
   actualizar(id) {
@@ -36,6 +39,13 @@ export default class GestionesInformes extends Component {
     HTTP.findAll("gestiones/inactivas").then((result) => {
       if (result !== false) {
         this.setState({ gestiones_nopublicadas: result });
+      }
+    });
+  }
+  getGestionesPublicadas() {
+    HTTP.findAll("gestiones/activas").then((result) => {
+      if (result !== false) {
+        this.setState({ gestiones_publicadas: result });
       }
     });
   }
@@ -54,6 +64,92 @@ export default class GestionesInformes extends Component {
       }
     });
   }
+  Publicar(codigo) {
+    Alerts.QuestionYesNo(
+      "¿Está seguro que desea publicar la gestión?",
+      "Administración de gestiones de informe",
+      "question"
+    ).then((resp) => {
+      if (resp) {
+        const data = {
+          codigoGestion: codigo,
+        };
+        Alerts.loading_reload(true);
+        Request.PUT("gestion/publicar", data).then((result) => {
+          Alerts.loading_reload(false);
+          if (result !== false) {
+            if (result.status === 200) {
+              Alerts.alertEmpty(
+                "¡Gestión publicada con éxito",
+                "Administración de gestiones de informe",
+                "success"
+              );
+              this.getGestionesNoPublicadas();
+              this.getGestionesPublicadas();
+              this.setState({
+                tab_active: "gestion_activa",
+              });
+            } else {
+              Alerts.alertEmpty(
+                "¡No se pudo publicar",
+                "Administración de gestiones de informe",
+                "error"
+              );
+            }
+          } else {
+            Alerts.alertEmpty(
+              "¡No se pudo publicar",
+              "Administración de gestiones de informe",
+              "error"
+            );
+          }
+        });
+      }
+    });
+  }
+  finalizar(codigo) {
+    Alerts.QuestionYesNo(
+      "¿Está seguro que desea finalizar la gestión?",
+      "Administración de gestiones de informe",
+      "question"
+    ).then((resp) => {
+      if (resp) {
+        const data = {
+          codigoGestion: codigo,
+        };
+        Alerts.loading_reload(true);
+        Request.PUT("gestion/cerrar", data).then((result) => {
+          Alerts.loading_reload(false);
+          if (result !== false) {
+            if (result.status === 200) {
+              Alerts.alertEmpty(
+                "¡Gestión finalizada con éxito",
+                "Administración de gestiones de informe",
+                "success"
+              );
+
+              this.getGestionesPublicadas();
+              this.setState({
+                tab_active: "gestion_activa",
+              });
+            } else {
+              Alerts.alertEmpty(
+                "¡No se pudo finalizar",
+                "Administración de gestiones de informe",
+                "error"
+              );
+            }
+          } else {
+            Alerts.alertEmpty(
+              "¡No se pudo finalizar",
+              "Administración de gestiones de informe",
+              "error"
+            );
+          }
+        });
+      }
+    });
+  }
   render() {
     return (
       <React.Fragment>
@@ -69,42 +165,76 @@ export default class GestionesInformes extends Component {
                   onSelect={this.handleChangeTab.bind(this)}
                   id="tabas"
                 >
-                  <Tab eventKey="gestion_activa" title="Gestión Activa">
+                  <Tab eventKey="gestion_activa" title="Gestiones Activas">
                     <div className="row">
                       <div className="col-lg-12 mt-4 ">
-                        <LayoutPanelEmpty titulo_panel="Gestion: GEST-1245454">
-                          <div className="row">
-                            <div className="col-lg-12 ">
-                              <table className="table table-bordered table-bordered-table color-bordered-table muted-bordered-table">
-                                <thead>
-                                  <tr>
-                                    <th>Tipo Gestión</th>
-                                    <th>Descripción</th>
-                                    <th>Fecha Publicación</th>
-                                    <th>Fecha inicio</th>
-                                    <th>Fecha Fin</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                  <tr>
-                                    <td>Informes de Iglesia</td>
-                                    <td>
-                                      Informes correspondiente al mes de Marzo
-                                      de 2021
-                                    </td>
-                                    <td>
-                                      Domingo, 14 de Marzo de 2021 - 4:35 AM
-                                    </td>
-                                    <td>Lunes, 15 Marzo de 2021 - 12:00 AM</td>
-                                    <td>
-                                      Miércoles, 15 Marzo de 2021 - 11:59 PM
-                                    </td>
-                                  </tr>
-                                </tbody>
-                              </table>
-                            </div>
+                        {this.state.gestiones_publicadas.length > 0 ? (
+                          this.state.gestiones_publicadas.map((element) => {
+                            return (
+                              <LayoutPanelEmpty
+                                titulo_panel={`Gestion: ${element.codigo}`}
+                                key={element.id}
+                                buttons={
+                                  <React.Fragment>
+                                    <button
+                                      type="button"
+                                      className="btn btn-outline-inverse"
+                                      onClick={this.finalizar.bind(
+                                        this,
+                                        element.codigo
+                                      )}
+                                    >
+                                      <i className="fa fa-close"></i> Finalizar
+                                    </button>
+                                  </React.Fragment>
+                                }
+                              >
+                                <div className="row">
+                                  <div className="col-lg-12">
+                                    <div className="table-responsive">
+                                      <table className="table table-bordered table-bordered-table color-bordered-table muted-bordered-table">
+                                        <thead>
+                                          <tr>
+                                            <th className="text-center">
+                                              Tipo Gestión
+                                            </th>
+                                            <th className="text-center">
+                                              Descripción
+                                            </th>
+                                            <th className="text-center">
+                                              Fecha Publicación
+                                            </th>
+                                            <th className="text-center">
+                                              Fecha Inicio
+                                            </th>
+                                            <th className="text-center">
+                                              Fecha Fin
+                                            </th>
+                                          </tr>
+                                        </thead>
+                                        <tbody>
+                                          <tr>
+                                            <td>{element.tipo_gestion_name}</td>
+                                            <td>{element.descripcion}</td>
+                                            <td>{element.fecha_publicacion}</td>
+                                            <td>
+                                              {element.fecha_recibir_inicio}
+                                            </td>
+                                            <td>{element.fecha_recibir_fin}</td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </div>
+                                  </div>
+                                </div>
+                              </LayoutPanelEmpty>
+                            );
+                          })
+                        ) : (
+                          <div className="col-lg-12 ">
+                            <h3>NO HAY NADA</h3>
                           </div>
-                        </LayoutPanelEmpty>
+                        )}
                       </div>
                     </div>
                   </Tab>
@@ -114,78 +244,95 @@ export default class GestionesInformes extends Component {
                   >
                     <div className="row">
                       <div className="col-lg-12 mt-4 ">
-                        {this.state.gestiones_nopublicadas.map((element) => {
-                          return (
-                            <LayoutPanelEmpty
-                              key={element.id}
-                              titulo_panel={`Gestion: ${element.codigo}`}
-                              buttons={
-                                <React.Fragment>
-                                  <button
-                                    onClick={this.actualizar.bind(
-                                      this,
-                                      element.id
-                                    )}
-                                    className="btn btn-outline-info mr-2"
-                                  >
-                                    <i className="fa fa-pencil mr-2"></i>
-                                    Actualizar
-                                  </button>
-                                  <button className="btn btn-outline-info mr-2">
-                                    <i className="fa fa-pencil mr-2"></i>
-                                    Publicar
-                                  </button>
-                                  <button
-                                    type="button"
-                                    onClick={this.delete.bind(
-                                      this,
-                                      element.codigo
-                                    )}
-                                    className="btn btn-outline-danger"
-                                  >
-                                    <i className="fa fa-pencil mr-2"></i>
-                                    Eliminar
-                                  </button>
-                                </React.Fragment>
-                              }
-                            >
-                              <div className="row">
-                                <div className="col-lg-12 ">
-                                  <table className="table table-bordered table-bordered-table color-bordered-table muted-bordered-table">
-                                    <thead>
-                                      <tr>
-                                        <th className="text-center">
-                                          Tipo Gestión
-                                        </th>
-                                        <th className="text-center">
-                                          Descripción
-                                        </th>
-                                        <th className="text-center">
-                                          Fecha Creación
-                                        </th>
-                                        <th className="text-center">
-                                          Fecha Inicio
-                                        </th>
-                                        <th className="text-center">
-                                          Fecha Fin
-                                        </th>
-                                      </tr>
-                                    </thead>
-                                    <tbody>
-                                      <tr>
-                                        <td>{element.tipo_gestion_name}</td>
-                                        <td>{element.descripcion}</td>
-                                        <td>{element.fecha_cr}</td>
-                                        <td>{element.fecha_recibir_inicio}</td>
-                                        <td>{element.fecha_recibir_fin}</td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
+                        {this.state.gestiones_nopublicadas.length > 0 ? (
+                          this.state.gestiones_nopublicadas.map((element) => {
+                            return (
+                              <LayoutPanelEmpty
+                                key={element.id}
+                                titulo_panel={`Gestion: ${element.codigo}`}
+                                buttons={
+                                  <React.Fragment>
+                                    <button
+                                      onClick={this.actualizar.bind(
+                                        this,
+                                        element.id
+                                      )}
+                                      className="btn btn-outline-info mr-2"
+                                    >
+                                      <i className="fa fa-pencil mr-2"></i>
+                                      Actualizar
+                                    </button>
+                                    <button
+                                      type="button"
+                                      onClick={this.Publicar.bind(
+                                        this,
+                                        element.codigo
+                                      )}
+                                      className="btn btn-outline-info mr-2"
+                                    >
+                                      <i className="fa fa-pencil mr-2"></i>
+                                      Publicar
+                                    </button>
+                                    <button
+                                      type="button"
+                                      onClick={this.delete.bind(
+                                        this,
+                                        element.codigo
+                                      )}
+                                      className="btn btn-outline-danger"
+                                    >
+                                      <i className="fa fa-pencil mr-2"></i>
+                                      Eliminar
+                                    </button>
+                                  </React.Fragment>
+                                }
+                              >
+                                <div className="row">
+                                  <div className="col-lg-12 ">
+                                    <div className="table-responsive">
+                                      <table className="table table-bordered table-bordered-table color-bordered-table muted-bordered-table">
+                                        <thead>
+                                          <tr>
+                                            <th className="text-center">
+                                              Tipo Gestión
+                                            </th>
+                                            <th className="text-center">
+                                              Descripción
+                                            </th>
+                                            <th className="text-center">
+                                              Fecha Creación
+                                            </th>
+                                            <th className="text-center">
+                                              Fecha Inicio
+                                            </th>
+                                            <th className="text-center">
+                                              Fecha Fin
+                                            </th>
+                                          </tr>
+                                        </thead>
+                                        <tbody>
+                                          <tr>
+                                            <td>{element.tipo_gestion_name}</td>
+                                            <td>{element.descripcion}</td>
+                                            <td>{element.fecha_cr}</td>
+                                            <td>
+                                              {element.fecha_recibir_inicio}
+                                            </td>
+                                            <td>{element.fecha_recibir_fin}</td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                            </LayoutPanelEmpty>
-                          );
-                        })}
+                              </LayoutPanelEmpty>
+                            );
+                          })
+                        ) : (
+                          <div className="col-lg-12 ">
+                            <h3>No hay nada</h3>
+                          </div>
+                        )}
                       </div>
                     </div>
                   </Tab>
